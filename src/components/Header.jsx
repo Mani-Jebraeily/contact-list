c4fdd13cd5dc209e5ae939809375feddf68105f3
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-02-13 04:27:37 +0330

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <>
+    <div className='flex justify-center flex-col  '>
+        <h1 className='  text-[35px] text-[#304ffe] mt-20 mb-5 m-auto '>Contact App</h1>
+        <p className=' m-auto text-gray-600'><a href='https://github.com/Mani-Jebraeily' className=' text-[#304ffe] bg-[#bbdefb] rounded-[5px] cursor-pointer hover:bg-[#304ffe] transition-all hover:text-[#bbdefb]'>Mani Jebraeily </a>| React.js</p>
+    </div>   
+    </>
+  )
+}
+
+export default Heading
