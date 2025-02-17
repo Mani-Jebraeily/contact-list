ab0e98deaf01b0f275cf59b0e25b4fa201cd7e15
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-02-17 16:16:10 +0330

    add responsive and styles

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -12,7 +12,15 @@ function Contacts() {
     phone:""
   })
 
-  const [contacts,setContacts]=useState([])
+  const [contacts,setContacts]=useState([
+    {
+      id:"1",
+      name:"Mani",
+      lastName:"Jebraeily",
+      email:"mani.jebraeily1382@gmail.com",
+      phone:"09195595961"
+    }
+  ])
   const [error,setError]=useState(false)
 
   const changeHandeler=(event)=>{
@@ -23,19 +31,20 @@ function Contacts() {
 
   const addHandeler=(event)=>{
     event.preventDefault()
-    if(!contact.name||!contact.lastName||!contact.email||!contact.phone){
+    if(!contact.name||!contact.lastName||!contact.email||contact.phone.length!==11){
       setError(true)
       return
     }else{setError(false)}
 
     const newContact={...contact,id: v4()}
     setContacts(contacts=>([...contacts,newContact]))
-    // setContact({
-    //   name:"",
-    //   lastName:"",
-    //   email:"",
-    //   phone:""
-    // })
+    setContact({
+      name:"",
+      lastName:"",
+      email:"",
+      phone:""
+    },
+  )
   }
 
    const deleteHandeler=(id)=>{
@@ -45,18 +54,18 @@ function Contacts() {
    }
   return (
     <>
-    <div className='bg- red-500 w-[1000px] h-[250px] mt-12 flex '>
+    <div className=' shadow-[0px_7px_29px_0px_#304ffe25] m-auto  rounded-2xl w-[400px] sm:w-[600px] lg:w-[900px] h-fit pb-5 pt-5 mt-12 flex '>
         <form  onSubmit={addHandeler} className='flex m-auto flex-row  gap-5 flex-wrap justify-center' >
-           <input type="text" name='name' value={contact.name} onChange={changeHandeler} placeholder='Last Name' className='w-[400px] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3'/>
-           <input type="text" name='lastName' value={contact.lastName} onChange={changeHandeler} placeholder='Name' className='w-[400px] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3' />
-           <input type="email" name='email' value={contact.email} onChange={changeHandeler} placeholder='Email' className='w-[400px] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3'/>
-           <input type="number" name='phone' value={contact.phone} onChange={changeHandeler}placeholder='Phone Number' className='w-[400px] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3' />
-           <button type='submit' className='w-[820px] h-[50px] border-gray-700 cursor-pointer border-[1px] rounded-xl'>Add Contact</button>
+           <input type="text" name='name' value={contact.name} onChange={changeHandeler} placeholder='Name' className='w-[90%] sm:w-[45%] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3'/>
+           <input type="text" name='lastName' value={contact.lastName} onChange={changeHandeler} placeholder=' Last Name' className='w-[90%] sm:w-[45%] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3' />
+           <input type="email" name='email' value={contact.email} onChange={changeHandeler} placeholder='Email' className='w-[90%] sm:w-[45%] h-[50px] border-gray-700 border-[1px] rounded-xl box-border pl-3'/>
+           <input type="number" name='phone' value={contact.phone} onChange={changeHandeler}placeholder='Phone Number' className='w-[90%] sm:w-[45%] h-[50px] border- border-[1px] rounded-xl box-border pl-3' />
+           <button type='submit' className='w-[90%]  sm:w-[calc(90%+20px)] h-[50px] bg-[#304ffe] text-[#fff] border-none cursor-pointer border-[1px] rounded-xl transition-all hover:shadow-[0px_7px_29px_0px_#0c2ad487]'>Add Contact</button>
         </form>
     </div>
     
-    <div className={error?"w-[1000px] h-[50px] bg-red-200 text-red-600 pl-3 box-border flex justify-left items-center rounded-2xl mt-5 mb-5":"invisible w-[1000px] h-[50px] mt-5 mb-5"}>
-       <h1>please enter valid data!</h1>
+    <div className={error?"w-[400px] sm:w-[600px] lg:w-[900px] h-[50px] m-auto bg-red-200 text-red-600 pl-3 box-border flex justify-left items-center rounded-2xl mt-5 mb-5":"invisible w-[1000px] h-[50px] mt-5 mb-5"}>
+       <h1>Please enter valid data!</h1>
     </div>
 
     <ContactsList contacts={contacts} deleteHandeler={deleteHandeler}/>
--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -5,15 +5,15 @@ function ContactsList({contacts,deleteHandeler}) {
 
   return (
     <>
-    <h1>Contacts List</h1>
-    <div className="w-[1000px] h-fit bg -red-300 p-10 box-border flex gap-4 items-center flex-col justify-evenly  ">
-        {contacts.length?contacts.map((contact)=>(<div className='w-[820px]  flex  justify-between bg-amber-500' key={contact.id}><p> {contact.lastName} {contact.name}</p>  <p><span>📬</span>{contact.email}</p> <p><span>📞</span>{contact.phone}</p> <button onClick={()=> deleteHandeler(contact.id)}>🗑️</button></div>))
-
-          :(<h1>No Contacts Yet!</h1>
-            
+    <div className='m-auto w-[400px] sm:w-[600px] lg:w-[900px]'>
+    <h1 className='text-[#304ffe] text-[1.5rem]'>Contacts List</h1>
+    <div className="w-[100%] h-fit  rounded-2xl  box-border sm:border-[#f5f5f5]  sm:border-[4px] flex  sm:gap-4 items-center flex-col justify-evenly  mb-10  mt-5 ">
+        {contacts.length?contacts.map((contact)=>(<div className='w-[98%] bg-[#f5f5f5] mb-[20px] pt-[10px] pb-[20px] pl-[20px] pr-[20px]  text-[#6b6b6f] rounded-xl flex flex-col  items-center gap-2 sm:flex-row sm:pb-[5px]  mt-[20px]   justify-between ' key={contact.id}> <p className='grow basis-0'> {contact.name} {contact.lastName} </p>  <p className='grow basis-0 flex '><span className='mr-[10px] bg-[#fff]  rounded-full '>📬</span>{contact.email}</p> <p className='grow basis-0 flex'><span className='mr-[10px]'>📞</span>{contact.phone}</p> <button onClick={()=> deleteHandeler(contact.id)} className=' hover:cursor-pointer bg-red-200 hover:shadow-[0px_18px_40px_0px_#00000022] rounded-xl size-[32px] ml-2 transition-all'>🗑️</button></div>))
+          :(<h1 className='bg-[#f5f5f5] w-[100%] h-[100px] flex justify-center items-center   rounded-2xl text-[#6b6b6f] text-[1.2rem] font-[500] '>No Contacts Yet!</h1>
           )
         }
 
+    </div>
     </div>
     </>
   )
